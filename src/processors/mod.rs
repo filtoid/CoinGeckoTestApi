a8f8c21f10186e9702a42472b18f34955d6ba7f7
filtoid <philjeffes@hotmail.com>
2023-10-08 18:25:20 +0200

    set CG delay via environment

--- src/processors/mod.rs
+++ src/processors/mod.rs
@@ -1,14 +1,18 @@
 use sqlx::postgres::PgPool;
 use std::{thread, time::Duration};
-
+use std::env;
+use log::debug;
 mod api_loader;
 
 pub async fn start_coin_updater_thread(pool: PgPool) {
+    let delay_string = env::var("CG_API_DELAY").expect("CG_API_DELAY must be set");
+    let delay: u64 = delay_string.parse().unwrap();
+    debug!("Delay between requests for CG Api {}", delay);
     loop {
 
         api_loader::refresh_coin_data(pool.clone()).await;
         
         // TODO: Read the pause value from env
-        thread::sleep(Duration::from_millis(5000));
+        thread::sleep(Duration::from_millis(delay));
     }
 }
